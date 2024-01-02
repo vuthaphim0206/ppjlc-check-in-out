89974d8c908331f955f33c7a9e928abd9fd11504
Vutha Phim <109074461+vuthaphim0206@users.noreply.github.com>
2024-01-03 01:02:16 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
-module.exports = {
-  /* other config options */
-  /* ... */
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-  // Add the exportPathmap function
-  exportPathMap: async function () {
-    return {
-      // Define your page routes here
-      '/': { page: '/' },
-      // Add more routes as needed
-    };
-  },
-  
-  // Add the basePath if your app is not hosted on the root of the domain
-  basePath: '/your-app-path',
-
-  // Configure the target as `serverless`
-  target: 'serverless',
-};
+module.exports = nextConfig
