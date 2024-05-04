e61d9c23275051ab8f1ef837a507f23b94d2e47d
Vutha Phim <109074461+vuthaphim0206@users.noreply.github.com>
2024-05-04 12:25:08 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="https://i.pinimg.com/736x/04/08/d6/0408d676048e0f7e271a335d8e3c6a4b.jpg" />
+    <meta name="google-site-verification" content="qJf15M0Lc3VzTqD2gFTrxghs2A0PGrf0UmXBgqsSQ08" />
       {/* <body className={inter.className}>{children}</body> */}
       <body className="absolute bg-slate-700 w-full m-auto ">
         {children}
