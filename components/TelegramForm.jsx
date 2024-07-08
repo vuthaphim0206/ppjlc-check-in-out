62b91f787002fba69408f9d17a126a97f9430785
Vutha Phim <109074461+vuthaphim0206@users.noreply.github.com>
2024-07-08 18:39:41 +0700

    Update TelegramForm.jsx

--- components/TelegramForm.jsx
+++ components/TelegramForm.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Modal from "./Modal";
+import Image from "next/image";
 
 export default function TelegramForm() {
   const [formData, setFormData] = useState({
